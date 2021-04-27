be348e3652633d4193b04c08ac1f937986860f9b
neg2led <4232981+neg2led@users.noreply.github.com>
2021-04-27 13:04:04 +1000

    reformat led

--- components/ws2812_led/ws2812_led.h
+++ components/ws2812_led/ws2812_led.h
@@ -8,37 +8,38 @@
 #pragma once
 #include <esp_err.h>
 
-/** Initialize the WS2812 RGB LED
+/**
+ * @brief Initialize the WS2812 RGB LED
  *
- * @return ESP_OK on success.
- * @return error in case of failure.
+ * @return esp_err_t, ESP_OK on success, <ERR_VAL> on error
  */
 esp_err_t led_init(void);
 
-/** Set RGB value for the WS2812 LED
+/**
+ * @brief Set RGB value for the WS2812 LED
  *
  * @param[in] red Intensity of Red color (0-100)
  * @param[in] green Intensity of Green color (0-100)
  * @param[in] blue Intensity of Green color (0-100)
  *
- * @return ESP_OK on success.
- * @return error in case of failure.
+ * @return esp_err_t, ESP_OK on success, <ERR_VAL> on error
  */
 esp_err_t led_set_rgb(uint32_t red, uint32_t green, uint32_t blue);
 
-/** Set HSV value for the WS2812 LED
+/**
+ * @brief Set HSV value for the WS2812 LED
  *
  * @param[in] hue Value of hue in arc degrees (0-360)
  * @param[in] saturation Saturation in percentage (0-100)
  * @param[in] value Value (also called Intensity) in percentage (0-100)
  *
- * @return ESP_OK on success.
- * @return error in case of failure.
+ * @return esp_err_t, ESP_OK on success, <ERR_VAL> on error
  */
 esp_err_t led_set_hsv(uint32_t hue, uint32_t saturation, uint32_t value);
 
-/** Clear (turn off) the WS2812 LED
- * @return ESP_OK on success.
- * @return error in case of failure.
+/**
+ * @brief Clear (turn off) the WS2812 LED
+ *
+ * @return esp_err_t, ESP_OK on success, <ERR_VAL> on error
  */
 esp_err_t led_clear(void);
